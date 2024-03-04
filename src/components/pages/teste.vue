e2ec7466eaea644586622d1c4f5c1b14ae2b661e
PauloBrasilTec <programador@brasiltec.ind.br>
2024-03-04 11:04:40 -0300

    ajuste de tela e inicio de carrinho

--- src/components/pages/teste.vue
+++ src/components/pages/teste.vue
@@ -1,4 +1,27 @@
 <template>
-
-    
-</template>
+    <div class="text-center">
+      <v-btn size="x-large" text="Click Me" @click="sheet = !sheet"></v-btn>
+      <v-bottom-sheet  v-model="sheet" class="bottom-sheet-right" inset>
+        
+      </v-bottom-sheet>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        sheet: false
+      }
+    }
+  }
+  </script>
+  
+  <style>
+    .bottom-sheet-right {
+      right: 0;
+      left: auto;
+      transform: translateX(100%);
+    }
+  </style>
+  
